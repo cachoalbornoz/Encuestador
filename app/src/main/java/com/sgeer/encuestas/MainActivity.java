c6dae64f5378d44004e16f6deee782c0ecde0b50
Guillermo Albornoz <cachoalbornoz@gmail.com>
2016-10-26 09:27:27 -0300

    Paquete Rosario - Boton Salir

--- app/src/main/java/com/sgeer/encuestas/MainActivity.java
+++ app/src/main/java/com/sgeer/encuestas/MainActivity.java
@@ -3,6 +3,7 @@ package com.sgeer.encuestas;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
@@ -10,21 +11,40 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 
+import static android.app.AlertDialog.THEME_HOLO_DARK;
 
-public class MainActivity extends Activity{
+
+public class MainActivity extends Activity {
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -34,7 +54,7 @@ public class MainActivity extends Activity{
 
         milocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, milocListener);
 
-        WebView webview = (WebView)findViewById(R.id.webView);
+        WebView webview = (WebView) findViewById(R.id.webView);
         webview.setWebChromeClient(new WebChromeClient());
 
         WebSettings webSettings = webview.getSettings();
@@ -43,6 +63,45 @@ public class MainActivity extends Activity{
 
         webview.addJavascriptInterface(new WebAppInterface(this), "Android");
         webview.loadUrl("file:///android_asset/encuesta.html");
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 
     public class WebAppInterface {
@@ -51,27 +110,29 @@ public class MainActivity extends Activity{
 
         WebAppInterface(Context c) {
             mContext = c;
-        };
+        }
+
+        ;
 
         @JavascriptInterface
         public void guardar(String texto) {
 
             String bestProvider;
-            LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+            LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             Criteria criteria = new Criteria();
-            bestProvider = lm.getBestProvider(criteria,false);
+            bestProvider = lm.getBestProvider(criteria, false);
 
             Location location = lm.getLastKnownLocation(bestProvider);
 
-            if(location == null){
-                Toast.makeText(getApplicationContext(), "Guardando respuesta",Toast.LENGTH_LONG ).show();
+            if (location == null) {
+                Toast.makeText(getApplicationContext(), "Guardando respuesta", Toast.LENGTH_LONG).show();
                 coordenadas = "0;0;";
 
-            }else{
-                Toast.makeText( getApplicationContext(),"Guardando posicion y respuesta", Toast.LENGTH_LONG ).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Guardando posicion y respuesta", Toast.LENGTH_LONG).show();
                 location.getLatitude();
                 location.getLongitude();
-                coordenadas = location.getLatitude()+ ";" + location.getLongitude() + ";";
+                coordenadas = location.getLatitude() + ";" + location.getLongitude() + ";";
             }
 
             String cadena = coordenadas + texto + '\n';
@@ -80,7 +141,8 @@ public class MainActivity extends Activity{
         }
 
         @JavascriptInterface
-        public void salir(){
+        public void salir() {
+
             AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(mContext);
             alertDialogBuilder.setTitle("Salir SGE V1.1?");
             alertDialogBuilder
@@ -103,16 +165,17 @@ public class MainActivity extends Activity{
 
             AlertDialog alertDialog = alertDialogBuilder.create();
             alertDialog.show();
+
         }
 
     }
 
-    private void IngresaRespuestas(String cadena){
+    private void IngresaRespuestas(String cadena) {
 
         String nombre_archivo = "respuestas.txt";
 
         try {
-            FileOutputStream fOut = new FileOutputStream("/sdcard/Download/" + nombre_archivo,true);
+            FileOutputStream fOut = new FileOutputStream("/sdcard/Download/" + nombre_archivo, true);
             OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
             myOutWriter.append(cadena);
             myOutWriter.close();
@@ -123,17 +186,19 @@ public class MainActivity extends Activity{
         }
     }
 
-    public class MiLocationListener implements LocationListener{
-        public void onLocationChanged(Location loc){
+    public class MiLocationListener implements LocationListener {
+        public void onLocationChanged(Location loc) {
         }
 
-        public void onProviderDisabled(String provider){
-            Toast.makeText( getApplicationContext(),"Active GPS por favor !", Toast.LENGTH_LONG ).show();
+        public void onProviderDisabled(String provider) {
+            Toast.makeText(getApplicationContext(), "Active GPS por favor !", Toast.LENGTH_LONG).show();
         }
 
-        public void onProviderEnabled(String provider){
-            Toast.makeText( getApplicationContext(),"Gps Activo", Toast.LENGTH_SHORT ).show();
+        public void onProviderEnabled(String provider) {
+            Toast.makeText(getApplicationContext(), "Gps Activo", Toast.LENGTH_SHORT).show();
         }
-        public void onStatusChanged(String provider, int status, Bundle extras){}
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
     }
 }
